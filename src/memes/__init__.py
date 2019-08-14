cb382da68f6059d86e9d085acdee8e463aa0b247
ropc <itslikeroar2412@gmail.com>
2019-08-14 00:18:24 -0400

    Add distracted bf meme

--- src/memes/__init__.py
+++ src/memes/__init__.py
@@ -27,5 +27,18 @@ ALL_MEMES = [
             DrawText(position=TextPosition.CUSTOM, customposition=Coordinate(x=316.5, y=256.5),
                 maxwidth=15, fontsize=24, fontcolor='black', hasborder=False)
         ]
+    ),
+    Meme(
+        image_filename='distracted-bf.jpg',
+        alias='distracted bf',
+        plugins=[
+            SplitText(separator='/'),
+            DrawText(position=TextPosition.CUSTOM, customposition=Coordinate(x=198, y=310),
+                maxwidth=15, fontsize=48, inputtextkey='text-0'),
+            DrawText(position=TextPosition.CUSTOM, customposition=Coordinate(x=472, y=153),
+                maxwidth=15, fontsize=48, inputtextkey='text-1'),
+            DrawText(position=TextPosition.CUSTOM, customposition=Coordinate(x=682, y=300),
+                maxwidth=15, fontsize=48, inputtextkey='text-2')
+        ]
     )
 ]
