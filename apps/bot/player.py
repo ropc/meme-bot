ad75f2e72a328913a2441a9df07d19b56ce8bdbb
ropc <itslikeroar2412@gmail.com>
2020-04-01 15:31:05 -0400

    scraping youtube instead of using api (because rate limits suck)

--- apps/bot/player.py
+++ apps/bot/player.py
@@ -9,6 +9,8 @@ import time
 import youtube_dl as yt
 import logging
 import uuid
+import urllib.parse
+import lxml.html
 from pydantic import BaseModel
 from typing import Dict, Deque, Callable, Awaitable, Union, Optional
 
@@ -75,7 +77,7 @@ class Player:
     async def enqueue(self, text: str):
         transaction_id = uuid.uuid4()
         async with self.queue_lock:
-            url = text if text.startswith('http') else await self._search(text)
+            url = text if text.startswith('http') else await self._search(text, transaction_id)
             if not url:
                 return
             item = await self._get_item(url, transaction_id)
@@ -142,9 +144,8 @@ class Player:
             ytdl.download([item.url])
 
 
-    async def _search(self, keyword: str) -> Optional[str]:
+    async def _search(self, keyword: str, transaction_id: str) -> Optional[str]:
         '''Searches for keyword and emits `SearchEvent`s'''
-        transaction_id = uuid.uuid4()
         result, _ = await asyncio.gather(
             _find_url(keyword),
             self.on_event_cb(SearchEvent(event_type=SearchEventType.SEARCHING, keyword=keyword, transaction_id=transaction_id))
@@ -157,7 +158,24 @@ class Player:
         return result
 
 
-async def _find_url(keyword: str) -> Optional[str]:
+async def _find_url_scrape(keyword: str) -> Optional[str]:
+    async with aiohttp.ClientSession() as session:  # type: aiohttp.ClientSession
+        log.debug(f'scraping youtube with keyword: {keyword}')
+        async with session.get(f'https://www.youtube.com/results?search_query={keyword}') as response:  # type: aiohttp.ClientResponse
+            html = lxml.html.fromstring(await response.text())
+            link_attributes = (e.attrib for e in html.xpath("//*[contains(@class, 'yt-uix-tile-link')]"))
+            for attributes in link_attributes:
+                href = attributes.get('href', '')
+                query_params = urllib.parse.parse_qs(urllib.parse.urlparse(href).query)
+                if len(query_params) != 1 or len(query_params.get('v', [])) != 1:
+                    continue
+                url = f"https://www.youtube.com/watch?v={query_params['v'][0]}"
+                log.debug(f'scrape youtube link for query "{keyword}": {url}')
+                return url
+    return None
+
+
+async def _find_url_api(keyword: str) -> Optional[str]:
     async with aiohttp.ClientSession() as session:  # type: aiohttp.ClientSession
         params = {
             'part': 'id',
@@ -171,8 +189,12 @@ async def _find_url(keyword: str) -> Optional[str]:
             content = await response.json()
             videoId = content.get('items', [{}])[0].get('id', {}).get('videoId')
             if not videoId:
-                log.warning(f'did not find videoId for keyword "{keyword}"')
+                log.warning(f'did not find videoId for query "{keyword}"')
                 return None
             url = f'https://www.youtube.com/watch?v={videoId}'
-            log.debug(f'youtube link for query "{keyword}": {url}')
+            log.debug(f'api youtube link for query "{keyword}": {url}')
             return url
+
+
+async def _find_url(keyword: str) -> Optional[str]:
+    return await _find_url_scrape(keyword)
