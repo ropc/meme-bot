ad735fb98bae5cd7b5acaa40872e31da089c65d9
ropc <itslikeroar2412@gmail.com>
2020-05-27 18:51:15 -0400

    add encrypted ssh key for deployment

---
+++ .github/scripts/decrypt_secret.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$SSH_KEY_PASSPHRASE" \
+--output $HOME/secrets/githubaction-key.pem $HOME/.github/scripts/githubaction-key.pem.gpg
