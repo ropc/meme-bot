0a67f877264bc8c49e30200a12a7204f7af54e0d
ropc <itslikeroar2412@gmail.com>
2020-05-26 23:47:42 -0400

    Allow playback commands only for users in voice channel

--- memebot/command.py
+++ memebot/command.py
@@ -7,12 +7,12 @@ from typing import Callable, Awaitable, Optional
 log = logging.getLogger('memebot')
 
 
-ExecutorCallable = Callable[[str, discord.abc.Messageable], Awaitable[None]]
+ExecutorCallable = Callable[['Command', discord.abc.Messageable], Awaitable[None]]
 
 
 class CommandExecutor(abc.ABC):
     @abc.abstractmethod
-    def __call__(self, command_arg: str, messageable: discord.abc.Messageable) -> Awaitable[None]:
+    def __call__(self, command: 'Command', messageable: discord.abc.Messageable) -> Awaitable[None]:
         pass
 
     @abc.abstractmethod
@@ -23,10 +23,16 @@ class CommandExecutor(abc.ABC):
 def executor(help_string: Optional[str]=None):
     def internal_executor(func: ExecutorCallable) -> CommandExecutor:
         class _CE(CommandExecutor):
-            def __call__(self, command_arg: str, messageable: discord.abc.Messageable) -> Awaitable[None]:
-                return func(command_arg, messageable)
+            def __call__(self, command: 'Command', messageable: discord.abc.Messageable) -> Awaitable[None]:
+                return func(command, messageable)
+
             def help_string(self) -> str:
-                return help_string if help_string else (func.__doc__ if func.__doc__ else '')
+                if help_string:
+                    return help_string
+                if func.__doc__:
+                    return func.__doc__
+                return ''
+
         return _CE()
     return internal_executor
 
@@ -35,6 +41,7 @@ class Command(BaseModel):
     name: str
     arg: str
     executor: CommandExecutor
+    initiator: discord.abc.User
     raw_command: str
 
     class Config:
@@ -42,7 +49,7 @@ class Command(BaseModel):
 
     async def execute(self, messageable: discord.abc.Messageable):
         try:
-            return await self.executor(self.arg, messageable)  # type: ignore
+            return await self.executor(self, messageable)
         except Exception:
             log.exception(f'error when executing command "{self.raw_command}"')
             await messageable.send(f'Something went wrong =(\nAttempted to use command "{self.name}": {self.executor.help_string()}')
