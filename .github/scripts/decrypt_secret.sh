0e139c90f50d49406be07b1f4cd58251aaee8f01
David Martinez <d@vidm.co>
2020-05-28 00:11:36 -0400

    change decrypt script

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SSH_KEY_PASSPHRASE" \
---output $HOME/secrets/githubaction-key.pem $HOME/.github/scripts/githubaction-key.pem.gpg
+--output $HOME/secrets/githubaction-key.pem $GITHUB_WORKSPACE/.github/scripts/githubaction-key.pem.gpg
