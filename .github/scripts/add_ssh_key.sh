c89a358d60485c8edf46254647698e5be479d997
David Martinez <d@vidm.co>
2020-05-28 01:22:47 -0400

    undoing additions for passphrase automation

--- .github/scripts/add_ssh_key.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect -f
-ls
-spawn ssh-add $HOME/secrets/githubaction-key.pem
-expect "Enter passphrase for $HOME/secrets/githubaction-key.pem:"
-send "$SSH_KEY_PASSPHRASE";
-expect "Identity added: $HOME/secrets/githubaction-key.pem ($HOME/secrets/githubaction-key.pem)"
-interact
