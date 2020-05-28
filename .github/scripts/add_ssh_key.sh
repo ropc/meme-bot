8a19ea383d295b53a26b56603405c8b94bf03869
David Martinez <d@vidm.co>
2020-05-28 01:09:29 -0400

    another script may work

---
+++ .github/scripts/add_ssh_key.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/expect -f
+spawn ssh-add $HOME/secrets/githubaction-key.pem
+expect "Enter passphrase for $HOME/secrets/githubaction-key.pem:"
+send "$SSH_KEY_PASSPHRASE";
+expect "Identity added: $HOME/secrets/githubaction-key.pem ($HOME/secrets/githubaction-key.pem)"
+interact
