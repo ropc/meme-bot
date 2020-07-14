066d9b3615fc6ac5f37fef5742aa5d0d3bb02e10
ropc <1666878+ropc@users.noreply.github.com>
2020-07-14 18:25:11 -0400

    fix for beans regex

--- memebot/cogs/beans.py
+++ memebot/cogs/beans.py
@@ -13,7 +13,7 @@ log = logging.getLogger('memebot')
 class Beans(commands.Cog):
     def __init__(self):
         self._ids: Set[int] = set(int(_id) for _id in os.getenv('MEME_BOT_BEANS_HOOK_IDS', '').split(','))
-        self._regex = re.compile(r'b\s*e\s*a\s*n(\s*s)?\b', flags=re.IGNORECASE)
+        self._regex = re.compile(r'\bb(\s*)e(\s*)a(\s*)n((\s*)s)?\b', flags=re.IGNORECASE)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
@@ -21,8 +21,8 @@ class Beans(commands.Cog):
             return
         if len(message.content) < 30:
             return  # too short, not worth commenting
-        match = self._regex.search(message.content)
-        if not match:
+        matches = self._regex.finditer(message.content)
+        if not any(_group_elements_match(match) for match in matches):
             return
         try:
             await message.add_reaction('\N{no entry}')
@@ -32,3 +32,9 @@ class Beans(commands.Cog):
         embed = format_quote_embed(message)
         embed.title = ':warning: WARNING: MESSAGE LOOKS LIKE A BEANS POST. READ AT YOUR OWN RISK :warning:'
         await message.channel.send(embed=embed)
+
+
+def _group_elements_match(match: re.Match):
+    bean_singular_spacing_matches = match.group(5) is None and match.group(1) == match.group(2) == match.group(3)
+    bean_plural_spacing_matches = match.group(1) == match.group(2) == match.group(3) == match.group(5)
+    return bean_singular_spacing_matches or bean_plural_spacing_matches
