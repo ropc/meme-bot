1c644a7d4e556df9f2f5739c49413cb3ad20329b
ropc <1666878+ropc@users.noreply.github.com>
2021-02-02 00:14:10 -0500

    fix pydantic validation error

--- meme_generator/plugins/splittext.py
+++ meme_generator/plugins/splittext.py
@@ -7,6 +7,9 @@ from .baseplugin import BasePlugin
 class SplitText(BasePlugin):
     regex = re.compile(r'(?:\s*/\s*|(https?://\S+))')
 
+    class Config:
+        arbitrary_types_allowed = True
+
     async def run(self, image: Image, text: str, context: Dict):
         non_empty_args = filter(None, self.regex.split(text))
         for (i, item) in enumerate(non_empty_args, 1):
