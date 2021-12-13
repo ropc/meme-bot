1b234f59438a15d5aa45e716ea8af9fd51c948c2
ropc <1666878+ropc@users.noreply.github.com>
2021-12-13 18:10:43 -0500

    Allow quoting using message id from other channels

--- memebot/cogs/quote.py
+++ memebot/cogs/quote.py
@@ -1,6 +1,18 @@
+import asyncio
 import re
 import discord
+from dataclasses import dataclass
+from typing import Callable, Optional
 from discord.ext import commands
+
+
+@dataclass
+class SearchParams:
+    channel_id: int
+    message_id: int
+
+
+ChannelProvider = Callable[[int], Optional[discord.Message]]
 
 
 class Quote(commands.Cog):
@@ -8,31 +20,39 @@ class Quote(commands.Cog):
     @commands.command()
     async def quote(self, context: commands.Context, arg: str):
         '''Quote a message. Usage: !quote <message id or message link>'''
+        searches = []
         if (match := re.match(r'https?://discord(app)?\.com/channels/(\d+)/(\d+)/(\d+)', arg)):
-            guild_id = int(match.group(2))
-            channel_id = int(match.group(3))
-            message_id = int(match.group(4))
+            searches.append(SearchParams(
+                channel_id=int(match.group(3)),
+                message_id=int(match.group(4))
+            ))
         else:
-            guild_id = context.channel.guild.id
-            channel_id = context.channel.id
             message_id = int(arg)
+            searches.extend(SearchParams(channel_id=c.id, message_id=message_id) for c in context.channel.guild.text_channels)
 
-        if (quote_channel := context.bot.get_channel(channel_id)) is None:
-            return await context.send(f'Cound not find channel with id {channel_id!r}')
-
-        try:
-            message = await quote_channel.fetch_message(message_id)
-        except (discord.NotFound, discord.Forbidden):
+        search_results = await asyncio.gather(*[fetch_message(context.bot.get_channel, sp.channel_id, sp.message_id) for sp in searches])
+        found_messages = [x for x in search_results if x]
+        if len(found_messages) == 0:
             return await context.send(f'Could not find message with id {message_id!r}')
 
-        await context.send(embed=format_quote_embed(message))
+        await context.send(embed=format_quote_embed(found_messages[0]))
+
+
+async def fetch_message(channel_provider: ChannelProvider, channel_id: int, message_id: int) -> Optional[discord.Message]:
+    if (quote_channel := channel_provider(channel_id)) is None:
+        return None
+
+    try:
+        return await quote_channel.fetch_message(message_id)
+    except (discord.NotFound, discord.Forbidden):
+        return None
 
 
 def format_quote_embed(message: discord.Message):
     quoted_content = '> ' + message.content.replace('\n', '\n> ')
     embed = discord.Embed(
         title=f'Link to message',
-        description=f'{quoted_content}\n - {message.author.name} in #{message.channel.name}',
+        description=f'{quoted_content}\n - {message.author.name} in {message.channel.mention}',
         url=create_message_link(message))
     embed.set_footer(text='Quote brought you by MORM-BET, the most reputable source for quotes. [citation needed]')
     return embed
