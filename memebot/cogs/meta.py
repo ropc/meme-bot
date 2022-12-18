8a587cb640be759aeae1a810bfa0966f5adb341b
ropc <1666878+ropc@users.noreply.github.com>
2022-12-18 10:24:52 -0500

    case insensitive role matching for !whois

--- memebot/cogs/meta.py
+++ memebot/cogs/meta.py
@@ -9,9 +9,43 @@ import tailer
 from discord.ext import commands
 from memebot.guildconfig import MemeBotConfig
 from typing import Optional
+from dataclasses import dataclass
 
 
 log = logging.getLogger('memebot')
+
+
+def name_equals_ignore_case(arg: str, role: discord.Role):
+    return role.name.lower() == arg.lower()
+
+
+# TODO: make this generic and work for both members and roles
+@dataclass
+class RoleMatch:
+    role: discord.Role
+    is_high_confindence: bool
+
+    @classmethod
+    async def convert(cls, ctx: commands.Context, argument):
+        '''note: this is only looking at cached roles, so may not always work'''
+        try:
+            return cls(
+                role=await commands.RoleConverter().convert(ctx, argument),
+                is_high_confindence=True,
+            )
+        except commands.RoleNotFound:
+            if not isinstance(argument, str) or not ctx.guild:
+                raise
+            roles = list(ctx.guild.roles)
+            matching_roles = [role for role in roles if name_equals_ignore_case(argument, role)]
+            if len(matching_roles) == 0:
+                raise
+            if len(matching_roles) > 1:  # too ambiguous
+                raise                      # maybe another interaction is possible?
+            return cls(
+                role=matching_roles[0],
+                is_high_confindence=len(ctx.guild.roles) > 0,  # assuming role isn't missing
+            )
 
 
 class Meta(commands.Cog):
@@ -42,8 +76,8 @@ class Meta(commands.Cog):
 
     @commands.command(aliases=['whois', 'who is'])
     @commands.guild_only()
-    async def who_is(self, context: commands.Context, role: discord.Role):
+    async def who_is(self, context: commands.Context, role_match: RoleMatch):
         '''Lists users with the given role'''
-        str_list = '\n'.join(f'- {member.mention}' for member in role.members)
-        str_message = f'{role.mention}:\n{str_list}'
+        str_list = '\n'.join(f'- {member.mention}' for member in role_match.role.members)
+        str_message = f'{role_match.role.mention}:\n{str_list}'
         await context.send(str_message, allowed_mentions=discord.AllowedMentions.none())
