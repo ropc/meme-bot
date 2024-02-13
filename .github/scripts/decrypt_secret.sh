fed037712a36be15ea96177297f704af7a4d2e5c
ropc <1666878+ropc@users.noreply.github.com>
2024-02-13 17:15:54 -0500

    update github actions

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command
-# --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SSH_KEY_PASSPHRASE" \
---output $HOME/secrets/githubaction-key.pem $GITHUB_WORKSPACE/.github/scripts/githubaction-key.pem.gpg
-chmod 600 $HOME/secrets/githubaction-key.pem
