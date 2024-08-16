21e8948361945054ba18f70d521cdd9cb5f8ee18
ropc <1666878+ropc@users.noreply.github.com>
2024-08-15 22:18:21 -0400

    working updated code to draw text

--- meme_generator/plugins/drawtext.py
+++ meme_generator/plugins/drawtext.py
@@ -19,40 +19,36 @@ class DrawText(BasePlugin):
     fontsize: int = 48
     textstyle: TextStyle = TextStyle.WHITE
 
-    async def run(self, image: Image, text: str, context: Dict):
-        text = textwrap.fill(text, self.maxwidth)
+    async def run(self, image: Image.Image, text: str, context: Dict):
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype('Impact', self.fontsize)
+        text = textwrap.fill(text, self.maxwidth)
 
-        position = self.get_text_position(image, draw, font, text)
+        textbox = draw.multiline_textbbox((0,0), text, font=font)
+        width = textbox[2]
+        height = textbox[3]
+
+        if self.position == AutoPosition.TOP:
+            drawposition = ((image.size[0] / 2) - (width / 2), image.size[1] * 0.05)
+        elif self.position == AutoPosition.CENTER:
+            drawposition = ((image.size[0] / 2) - (width / 2), (image.size[1] * 0.5) - (height / 2))
+        elif self.position == AutoPosition.BOTTOM:
+            drawposition = ((image.size[0] / 2) - (width / 2), (image.size[1] * 0.95) - height)
+        elif isinstance(self.position, Coordinate):
+            drawposition = (self.position.x - (width / 2), self.position.y - (height / 2))
 
         if self.textstyle == TextStyle.WHITE:
-            draw_outlined_text(draw, position, text, font=font, fill='white')
+            draw_outlined_text(draw, drawposition, text, font=font)
         else:
-            draw.multiline_text(position, text, align='center', font=font, fill='black')
-
-    def get_text_position(self, image: Image.Image, draw: ImageDraw.ImageDraw, font: ImageFont.ImageFont, text: str) -> Tuple[float, float]:
-        center = (image.size[0] / 2, image.size[1] / 2)
-        position_centered_x, position_centered_y = find_centered_position(center, draw.textsize(text, font=font))
-
-        if isinstance(self.position, Coordinate):
-            return find_centered_position((self.position.x, self.position.y), draw.textsize(text, font=font))
-        elif self.position == AutoPosition.TOP:
-            position_y = image.size[1] * 0.05
-        elif self.position == AutoPosition.CENTER:
-            position_y = position_centered_y
-        elif self.position == AutoPosition.BOTTOM:
-            position_y = (image.size[1] * 0.95) - draw.textsize(text, font=font)[1]
-
-        return (position_centered_x, position_y)
+            draw.multiline_text(drawposition, text, fill='black', align='center', spacing=4, font=font)
 
 
-def draw_outlined_text(draw: ImageDraw, position, text: str, **kwargs):
+def draw_outlined_text(draw: ImageDraw.ImageDraw, position, text: str, **kwargs):
     x, y = position
-    shadowcolor = 'black'
     nonfill_kargs = {k:v for (k, v) in kwargs.items() if k != 'fill'}  # gross
-    draw.multiline_text((x-1, y-1), text, fill=shadowcolor, align='center', spacing=4, **nonfill_kargs)
-    draw.multiline_text((x+1, y-1), text, fill=shadowcolor, align='center', spacing=4, **nonfill_kargs)
-    draw.multiline_text((x-1, y+1), text, fill=shadowcolor, align='center', spacing=4, **nonfill_kargs)
-    draw.multiline_text((x+1, y+1), text, fill=shadowcolor, align='center', spacing=4, **nonfill_kargs)
-    draw.multiline_text(position, text, align='center', spacing=4, **kwargs)
+    draw.multiline_text((x-1, y-1), text, fill='black', align='center', spacing=4, **nonfill_kargs)
+    draw.multiline_text((x+1, y-1), text, fill='black', align='center', spacing=4, **nonfill_kargs)
+    draw.multiline_text((x-1, y+1), text, fill='black', align='center', spacing=4, **nonfill_kargs)
+    draw.multiline_text((x+1, y+1), text, fill='black', align='center', spacing=4, **nonfill_kargs)
+    draw.multiline_text(position, text, fill='white', align='center', spacing=4, **kwargs)
+
